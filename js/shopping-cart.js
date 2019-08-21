438788adf3638151bc0ddc29a1bf82a1cf996b93
Josh Bivens <thejoshbivens@gmail.com>
2019-08-21 11:22:51 -0600

    Print item retrieved from Firestore to page

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,12 +1,32 @@
-/*
+const items = document.querySelector("#items");
 
---- Modal HTML ---
-<div class="modal-content">
-  <h4>Modal Header</h4>
-  <p>A bunch of text</p>
-</div>
-<div class="modal-footer">
-  <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-</div> 
-
-*/
+// Fill storefront
+db.collection("items").get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      const item = doc.data();
+      items.innerHTML += `
+        <div class="col s12 m6">
+          <div class="card medium">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://picsum.photos/200">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">${item.name}
+                <i class="material-icons right">more_vert</i>
+              </span>
+              <p>${item.short_desc}</p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">${item.name}
+                <i class="material-icons right">close</i>
+              </span>
+              <h5>$${item.price}.00</h5>
+              <p>${item.description}</p>
+              <p><i>Free shipping on orders over $50!</i></p>
+            </div>
+          </div>
+        </div>
+      `
+    });
+  });
